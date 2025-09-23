239f889202113bbd8e4500a3a12d2004dc213e7a
philipbreen <69089848+philipbreen@users.noreply.github.com>
2025-09-23 15:27:31 -0400

    Update TestingLargeDC.ino

--- TestingLargeDC.ino
+++ TestingLargeDC.ino
@@ -28,9 +28,51 @@ void setup() {
 
 void loop() {
 delay(2000);
-  analogWrite(ALIN_PIN, 255);
-  analogWrite(AHIN_PIN, 126);
+pwmWrite10kHz(ALIN_PIN, 0);   // 75% duty on pin 9
+pwmWrite10kHz(AHIN_PIN, 0);  // 10% duty on pin 10
   delay(2000);
-    analogWrite(ALIN_PIN, 255);
-  analogWrite(AHIN_PIN, 0);
+
+pwmWrite10kHz(ALIN_PIN, 50);   // 75% duty on pin 9
+pwmWrite10kHz(AHIN_PIN, 50);  // 10% duty on pin 10
+}
+
+
+void pwmWrite10kHz(uint8_t pin, uint8_t dutyCyclePercent) {
+  // Limit duty cycle to 0–100%
+  if (dutyCyclePercent > 100) dutyCyclePercent = 100;
+
+  // Configure Timer1 only once
+  static bool initialized = false;
+  if (!initialized) {
+    // Reset Timer1 control registers
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1  = 0;
+
+    // Fast PWM mode with ICR1 as TOP
+    TCCR1A |= (1 << WGM11);
+    TCCR1B |= (1 << WGM12) | (1 << WGM13);
+
+    // Non-inverting mode on OC1A (pin 9) and OC1B (pin 10)
+    // We'll enable them individually below
+    // Prescaler = 8
+    TCCR1B |= (1 << CS11);
+
+    // Set TOP value for 10 kHz
+    // fPWM = 16MHz / (prescaler * (1 + ICR1))
+    // ICR1 = (16MHz / (8 * 10kHz)) - 1 = 199
+    ICR1 = 199;
+
+    initialized = true;
+  }
+
+  if (pin == 9) {
+    pinMode(9, OUTPUT);
+    TCCR1A |= (1 << COM1A1);  // enable output on pin 9
+    OCR1A = (dutyCyclePercent * ICR1) / 100;
+  } else if (pin == 10) {
+    pinMode(10, OUTPUT);
+    TCCR1A |= (1 << COM1B1);  // enable output on pin 10
+    OCR1B = (dutyCyclePercent * ICR1) / 100;
+  }
 }
